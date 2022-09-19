a1da61424c06a4b177918a017fcc3cfa06825c12
kaldan007 <kaldantenzin@gmail.com>
2022-09-19 17:05:14 +0530

    remove reimport config class

--- src/config.py
+++ src/config.py
@@ -1,15 +1,10 @@
 class BaseConfig:
-    pass
-class ImportConfig(BaseConfig):
-
-    def __init__(self, ocr_engine: str, model_type: str = None) -> None:
-        self.ocr_engine = ocr_engine
-        self.model_type = model_type
-        
-
-
-class ReimportConfig(BaseConfig):
-    
     def __init__(self, formatter_type: str) -> None:
         self.formatter_type = formatter_type
-    
+
+class ImportConfig(BaseConfig):
+
+    def __init__(self, formatter_type: str, ocr_engine: str, model_type: str = None) -> None:
+        super().__init__(formatter_type=formatter_type)
+        self.ocr_engine = ocr_engine
+        self.model_type = model_type
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,24 +1,19 @@
-from src.config import BaseConfig, ImportConfig, ReimportConfig
+from src.config import BaseConfig, ImportConfig
 
 def test_base_config():
+    formatter_type = "google_vision"
 
-    base_config = BaseConfig()
+    base_config = BaseConfig(formatter_type=formatter_type)
 
-    assert base_config
+    assert base_config.formatter_type == formatter_type
 
 def test_import_config():
     ocr_engine = "google_vision"
+    formatter_type = "google_vision"
     model_type = "weekly-built-in"
 
-    import_config = ImportConfig(ocr_engine=ocr_engine, model_type=model_type)
+    import_config = ImportConfig(formatter_type=formatter_type, ocr_engine=ocr_engine, model_type=model_type)
 
     assert import_config.ocr_engine == ocr_engine
     assert import_config.model_type == model_type
-    
-
-def test_reimport_config(): 
-    formatter_type = "namsel_ocr"
-
-    reimport_config = ReimportConfig(formatter_type=formatter_type)
-
-    assert reimport_config.formatter_type == formatter_type
+    assert import_config.formatter_type == formatter_type
