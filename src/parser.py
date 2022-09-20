26732f51d9416e6738bfd1ffe2d67685627ce219
kaldan007 <kaldantenzin@gmail.com>
2022-09-20 11:40:17 +0530

    update parser

--- src/parser.py
+++ src/parser.py
@@ -1,21 +1,28 @@
 from openpecha.formatters.google_ocr import GoogleOCRFormatter
-from openpecha.core.ids import get_initial_pecha_id
-from src.config import BaseConfig
 
+from src.config import BaseConfig, GOOGLE_VISION_PARSER_LINK
+from src.exceptions import OCREngineNotSupported
+
+
+PARSERS_REGISTER = {
+    "google_vision": GoogleOCRFormatter,
+}
 class OCRParser:
 
-    def __init__(self, config:BaseConfig, ocr_output_path:str, opf_dir:str, pecha_id=None) -> None:
+    def __init__(self, config:BaseConfig, ocr_output_path:str, opf_dir:str, pecha_id=None, parsers_register=PARSERS_REGISTER) -> None:
         self.config = config
         self.ocr_output_path = ocr_output_path
         self.opf_dir = opf_dir
-        if pecha_id:
-            self.pecha_id = pecha_id
-        else:
-            self.pecha_id = get_initial_pecha_id()
+        self.parsers_register = parsers_register
+        self.pecha_id = pecha_id
 
     def parse(self):
-        opf_path = None
-        if self.config.formatter_type == "google_vision":
-            formatter = GoogleOCRFormatter(output_path=self.opf_dir)
-            opf_path = formatter.create_opf(self.ocr_output_path, self.pecha_id)
+        try:
+            formatter_class = self.parsers_register[self.config.ocr_engine]
+        except OCREngineNotSupported:
+            raise OCREngineNotSupported(
+                f'{self.config.ocr_engine} not supported. Please select from {PARSERS_REGISTER.keys()}'
+            )
+        formatter = formatter_class(output_path=self.opf_dir)
+        opf_path = formatter.create_opf(input_path=self.ocr_output_path, pecha_id=self.pecha_id, parser_link=GOOGLE_VISION_PARSER_LINK)
         return opf_path
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,28 +1,35 @@
-from src.config import BaseConfig
+from pathlib import Path
+
+from src.config import ReimportConfig, ImportConfig
 from src.parser import OCRParser
 
-def test_ocr_parser():
-    config = BaseConfig(formatter_type="google_vision")
-    ocr_output_path = "./tests/data/google_vision/ocr_output/"
-    opf_dir = "./tests/data/google_vision/opfs/"
-    pecha_id = "I123456"
+class MockOCRFormatter:
 
-    parser = OCRParser(config=config, ocr_output_path=ocr_output_path, opf_dir=opf_dir, pecha_id=pecha_id)
+    def __init__(self, *args, **kwargs) -> None:
+        pass
 
-    assert parser.config == config
-    assert parser.ocr_output_path == ocr_output_path
-    assert parser.opf_dir == opf_dir
-    assert parser.pecha_id == pecha_id
+    def create_opf(self, *args, **kwargs):
+        return Path.home()
+        
 
+def test_ocr_parser_with_import_config():
+    config = ImportConfig(ocr_engine="mock_ocr")
+    parsers_register = {
+        'mock_ocr': MockOCRFormatter
+    }
 
-# def test_google_parser():
-#     config = BaseConfig(formatter_type="google_vision")
-#     ocr_output_path = "./tests/data/google_vision/ocr_output/"
-#     opf_dir = "./tests/data/google_vision/opfs/"
-#     pecha_id = "I123456"
+    parser = OCRParser(config=config, ocr_output_path=None, opf_dir=None, pecha_id=None, parsers_register=parsers_register)
+    pecha_path = parser.parse()
 
+    assert isinstance(pecha_path, Path)
 
-#     parser = OCRParser(config=config, ocr_output_path=ocr_output_path, opf_dir=opf_dir, pecha_id=pecha_id)
-#     opf_path = parser.parse()
-    
-#     assert opf_path == f"{opf_dir}/{pecha_id}" 
+def test_ocr_parser_with_reimport_config():
+    config = ReimportConfig(ocr_engine="mock_ocr")
+    parsers_register = {
+        'mock_ocr': MockOCRFormatter
+    }
+
+    parser = OCRParser(config=config, ocr_output_path=None, opf_dir=None, pecha_id=None, parsers_register=parsers_register)
+    pecha_path = parser.parse()
+
+    assert isinstance(pecha_path, Path)
