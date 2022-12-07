70c5b7b912f72e94a2528e1c27c681b05adc91f7
10zinten <ten13zin@gmail.com>
2022-12-07 14:45:28 +0530

    fix: hierarchy of exceptions

--- ocr_pipelines/exceptions.py
+++ ocr_pipelines/exceptions.py
@@ -1,16 +1,36 @@
-class OCREngineNotSupported(Exception):
-    pass
+class Error(Exception):
+    """Base-class for all exceptions raised by the package"""
 
 
-class BdcrScanNotFound(Exception):
-    pass
+class ParserError(Error):
+    """Base-class for parser errors."""
 
 
-class ImageGroupNotFound(Exception):
-    pass
+class OcrEngineError(Error):
+    """Base-class for OCR engine errors."""
 
 
-class DataProviderNotSupported(Exception):
+class ImageDownloaderError(Error):
+    """Base-class for image downloader errors."""
+
+
+class PipelineError(Error):
+    """Base-class for pipeline errors."""
+
+
+class OCREngineNotSupported(OcrEngineError):
+    """Specified OCR engine is not supported."""
+
+
+class BdcrScanNotFound(ImageDownloaderError):
+    """A provided BDRC scan id is not found."""
+
+
+class ImageGroupNotFound(ImageDownloaderError):
+    """A provided bdrc imagegroup is not found."""
+
+
+class DataProviderNotSupported(ParserError):
     pass
 
 
