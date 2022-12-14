cfad5bb1a5077663d958b9d9c89023d7d550036e
10zinten <ten13zin@gmail.com>
2022-12-14 11:27:31 +0530

    fix: create BdrcS3Uploader class

--- ocr_pipelines/upload.py
+++ ocr_pipelines/upload.py
@@ -1,5 +1,4 @@
 import hashlib
-import os
 from pathlib import Path
 
 import boto3
@@ -11,41 +10,38 @@ S3 = boto3.resource("s3")
 S3_client = boto3.client("s3")
 ocr_output_bucket = S3.Bucket(OCR_OUTPUT_BUCKET)
 
-ORG_NAME = "Openpecha-data"
 
-TOKEN = os.environ["GITHUB_TOKEN"] if "GITHUB_TOKEN" in os.environ else ""
+class BdrcS3Uploader:
+    """Uploads the ocr output to s3"""
 
+    def __init__(self, bdrc_scan_id: str, service: str, batch: str):
+        self.bdrc_scan_id = bdrc_scan_id
+        self.service = service
+        self.batch = batch
 
-def get_bdrc_scan_id_hash(bdrc_scan_id: str) -> str:
-    return hashlib.md5(bdrc_scan_id.encode("utf-8")).hexdigest()[:2]
+    def __get_first_two_chars_hash(self, string) -> str:
+        return hashlib.md5(string.encode("utf-8")).hexdigest()[:2]
 
+    @staticmethod
+    def __get_s3_suffix_for_imagegroup(imagegroup: str) -> str:
+        pre, rest = imagegroup[0], imagegroup[1:]
+        if pre == "I" and rest.isdigit() and len(rest) == 4:
+            return rest
+        return imagegroup
 
-def get_s3_suffix_for_imagegroup(imagegroup: str) -> str:
-    pre, rest = imagegroup[0], imagegroup[1:]
-    if pre == "I" and rest.isdigit() and len(rest) == 4:
-        return rest
-    return imagegroup
+    @property
+    def base_dir(self) -> Path:
+        """Returns the base dir to store the ocr outputs
 
+        the function can be inspired from
+        https://github.com/buda-base/volume-manifest-tool/blob/f8b495d908b8de66ef78665f1375f9fed13f6b9c/manifestforwork.py#L94
+        """
+        hash_ = self.__get_first_two_chars_hash(self.bdrc_scan_id)
+        return Path("Works") / hash_ / self.bdrc_scan_id / self.service / self.batch
 
-def get_s3_path_prefix(bdrc_scan_id: str, imagegroup: str, service: str, batch: str):
-    """Returns the s3 prefix for the given bdrc_scan_id and imagegroup
-
-    the function can be inspired from
-    https://github.com/buda-base/volume-manifest-tool/blob/f8b495d908b8de66ef78665f1375f9fed13f6b9c/manifestforwork.py#L94
-
-    Args:
-        bdrc_scan_id (str): bdrc scan id (eg: W22084)
-        imagegroup (str): imagegroup (eg: I0886)
-        service (str): service (eg: google-vision)
-        batch_id (str): batch id
-
-    Returns:
-        str: s3 prefix (folder)
-    """
-    hash_ = get_bdrc_scan_id_hash(bdrc_scan_id)
-    imagegroup_suffix = get_s3_suffix_for_imagegroup(imagegroup)
-
-    return f"Works/{hash_}/{bdrc_scan_id}/{service}/{batch}/{bdrc_scan_id}-{imagegroup_suffix}"
+    def get_imagegroup_dir(self, imagegroup: str) -> Path:
+        imagegroup_suffix = self.__get_s3_suffix_for_imagegroup(imagegroup)
+        return self.base_dir / f"{self.bdrc_scan_id}-{imagegroup_suffix}"
 
 
 def is_archived(key):
@@ -65,10 +61,9 @@ def save_to_s3(path: Path, service: str, batch: str) -> None:
         batch_id (str): batch id
     """
 
-    bdrc_scan_id = path.stem
     for img_group_dir in path.iterdir():
-        img_group_id = img_group_dir.stem
-        s3_path_prefix = get_s3_path_prefix(bdrc_scan_id, img_group_id, service, batch)
+        # s3_path_prefix = get_s3_path_prefix(bdrc_scan_id, img_group_id, service, batch)
+        s3_path_prefix = ""
         for ocr_output_file in img_group_dir.iterdir():
             s3_output_path = f"{s3_path_prefix}/{ocr_output_file.name}"
             if is_archived(s3_output_path):
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,18 +1,19 @@
+from pathlib import Path
+
 import pytest
 
-from ocr_pipelines.upload import (
-    get_bdrc_scan_id_hash,
-    get_s3_path_prefix,
-    get_s3_suffix_for_imagegroup,
-)
+from ocr_pipelines.upload import BdrcS3Uploader
 
 
-def test_get_bdrc_scan_id_hash():
+def test_get_frist_two_chars_hash():
     # arrange
     bdrc_scan_id = "W1KG12345"
+    service = "google-vision"
+    batch = "batch-1"
+    uploader = BdrcS3Uploader(bdrc_scan_id, service, batch)
 
     # act
-    bdrc_scan_id_hash = get_bdrc_scan_id_hash(bdrc_scan_id)
+    bdrc_scan_id_hash = uploader._BdrcS3Uploader__get_first_two_chars_hash(bdrc_scan_id)  # type: ignore
 
     # assert
     assert bdrc_scan_id_hash == "67"
@@ -27,22 +28,45 @@ def test_get_bdrc_scan_id_hash():
     ],
 )
 def test_get_s3_suffix_for_imagegroup(imagegroup, expected):
+    # arrange
+    bdrc_scan_id = "W1KG12345"
+    service = "google-vision"
+    batch = "batch-1"
+    uploader = BdrcS3Uploader(bdrc_scan_id, service, batch)
+
     # act
-    s3_suffix = get_s3_suffix_for_imagegroup(imagegroup)
+    s3_suffix = uploader._BdrcS3Uploader__get_s3_suffix_for_imagegroup(imagegroup)  # type: ignore
 
     # assert
     assert s3_suffix == expected
 
 
-def test_get_s3_path_prefix():
+def test_base_dir():
     # arrange
     bdrc_scan_id = "W1KG12345"
-    imagegroup = "I1234"
     service = "google-vision"
     batch = "batch-1"
+    uploader = BdrcS3Uploader(bdrc_scan_id, service, batch)
 
     # act
-    s3_path_prefix = get_s3_path_prefix(bdrc_scan_id, imagegroup, service, batch)
+    base_dir = uploader.base_dir
 
     # assert
-    assert s3_path_prefix == "Works/67/W1KG12345/google-vision/batch-1/W1KG12345-1234"
+    assert base_dir == Path("Works/67/W1KG12345/google-vision/batch-1")
+
+
+def test_imagegroup_dir():
+    # arrange
+    bdrc_scan_id = "W1KG12345"
+    service = "google-vision"
+    batch = "batch-1"
+    imagegroup = "I1234"
+    uploader = BdrcS3Uploader(bdrc_scan_id, service, batch)
+
+    # act
+    imagegroup_dir = uploader.get_imagegroup_dir(imagegroup)
+
+    # assert
+    assert imagegroup_dir == Path(
+        "Works/67/W1KG12345/google-vision/batch-1/W1KG12345-1234"
+    )
