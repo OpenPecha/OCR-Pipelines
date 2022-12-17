b346f89ec6b25248c3e2514c56927e9ac62c491b
10zinten <ten13zin@gmail.com>
2022-12-17 11:53:05 +0530

    test: parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,19 +1,19 @@
-
 import tempfile
 from pathlib import Path
 
 from openpecha.core.pecha import OpenPechaFS
 
-from ocr_pipelines.config import ReimportConfig, ImportConfig
+from ocr_pipelines.config import ImportConfig, ReimportConfig
 from ocr_pipelines.parser import OCRParser
 
-class MockOCRFormatter:
 
+class MockOCRFormatter:
     def __init__(self, *args, **kwargs) -> None:
         pass
 
     def create_opf(self, *args, **kwargs):
-        return OpenPechaFS()
+        return OpenPechaFS(path=Path("/tmp"))
+
 
 class MockDataProvider:
     def __init__(self, bdrc_scan_id, ocr_import_info, ocr_disk_path=None, mode="local"):
@@ -22,36 +22,43 @@ class MockDataProvider:
         self.ocr_disk_path = ocr_disk_path
         self.bdrc_scan_id = bdrc_scan_id
         self.mode = mode
-        
+
 
 def test_ocr_parser_with_import_config():
     config = ImportConfig(ocr_engine="mock_ocr")
-    parsers_register = {
-        'mock_ocr': MockOCRFormatter
-    }
-    data_provider_register = {
-        'mock_ocr': MockDataProvider
-    }
-    
+    parsers_register = {"mock_ocr": MockOCRFormatter}
+    data_provider_register = {"mock_ocr": MockDataProvider}
+
     with tempfile.TemporaryDirectory() as tmpdirname:
         ocr_output_path = Path(tmpdirname) / "W123456"
-        parser = OCRParser(config=config, ocr_output_path=ocr_output_path, opf_dir=None, pecha_id=None, parsers_register=parsers_register, data_provider_register=data_provider_register)
+        parser = OCRParser(
+            config=config,
+            ocr_output_path=ocr_output_path,
+            opf_dir="",
+            pecha_id=None,
+            parsers_register=parsers_register,
+            data_provider_register=data_provider_register,
+        )
         pecha = parser.parse()
 
         assert isinstance(pecha, OpenPechaFS)
 
+
 def test_ocr_parser_with_reimport_config():
     config = ReimportConfig(ocr_engine="mock_ocr")
-    parsers_register = {
-        'mock_ocr': MockOCRFormatter
-    }
-    data_provider_register = {
-        'mock_ocr': MockDataProvider
-    }
+    parsers_register = {"mock_ocr": MockOCRFormatter}
+    data_provider_register = {"mock_ocr": MockDataProvider}
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         ocr_output_path = Path(tmpdirname) / "W123456"
-        parser = OCRParser(config=config, ocr_output_path=ocr_output_path, opf_dir=None, pecha_id=None, parsers_register=parsers_register, data_provider_register=data_provider_register)
+        parser = OCRParser(
+            config=config,
+            ocr_output_path=ocr_output_path,
+            opf_dir="",
+            pecha_id=None,
+            parsers_register=parsers_register,
+            data_provider_register=data_provider_register,
+        )
         pecha = parser.parse()
 
         assert isinstance(pecha, OpenPechaFS)
